6d99908d98f71439796fdfa84200426a171f509c
Sourabh Chougule <sourabhchg2141@gmail.com>
2020-11-08 12:13:45 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,126 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+
+var boxId = 100;
+
+function myBox() { 
+
+  var uBoxId = boxId++;
+  var zindex = uBoxId;
+  
+  var box = document.createElement("button");
+  var boxName = document.createTextNode('Box ' + uBoxId);
+  box.appendChild(boxName);
+
+  box.className = "btn btn-primary";
+  box.style.margin = "2px";
+  box.style.height = "100px";
+  box.style.width = "100px";
+  box.style.position = "relative";
+  box.id = (uBoxId).toString();
+  box.setAttribute("z-index",zindex.toString());
+  box.addEventListener("click",onBoxClicked);
+
+  var elem = document.getElementById("addBox");   
+  elem.appendChild(box);   
+
+  function onBoxClicked(){
+
+    document.getElementById(uBoxId.toString()).onkeydown = function(e){
+      
+      switch (e.key) { 
+        case "ArrowLeft": 
+            moveLeft();
+            break;
+        case "a": 
+            moveLeft();
+            break;
+        case "A":
+            moveLeft();
+            break;     
+        case "ArrowRight": 
+            moveRight();
+            break;
+        case "d": 
+            moveRight();
+            break;
+        case "D": 
+            moveRight();
+            break;         
+        case "ArrowUp": 
+            moveUp();
+            break;
+        case "w": 
+            moveUp();
+            break;
+        case "W": 
+            moveUp();
+            break;         
+        case "ArrowDown": 
+            moveDown();
+            break;
+        case "s": 
+            moveDown();
+            break;
+        case "S": 
+            moveDown();
+            break; 
+        case "Delete":
+            deleteBox();
+            break;
+      } 
+    };
+
+  }
+
+  function moveLeft(){    
+    console.log('left');
+    box.style.left = '-40px';      
+  }
+
+  function moveRight(){
+    console.log('right');
+    box.style.left = '+40px';   
+  }
+
+  function moveUp(){
+    console.log('up');
+    box.style.top = '-40px';
+  }
+
+  function moveDown(){
+    console.log('down');
+    box.style.top = '+40px'
+  }
+
+  function deleteBox(){
+    console.log('delete');
+    var remove = document.getElementById(uBoxId.toString());
+    remove.remove();
+  }
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 
-  ngOnInit(){
-  }
+  checkboxFlag: boolean;
 
-  @ViewChild('div') div: ElementRef;
-  constructor (private renderer: Renderer2){
-  }
-  boxId: number = 101;
-  
-  onAdd() {
-    
-    this.boxId = this.boxId + 1;
-
-    var button: HTMLButtonElement = this.renderer.createElement('button'); 
-    button.setAttribute('class','btn btn-primary box');
-    button.setAttribute('type','button');
-    button.setAttribute('id',this.boxId.toString());  //unique id number
-    button.setAttribute('z-index',this.boxId.toString());  // z index
-    button.setAttribute('position','relative');
-    button.setAttribute('value','Box' + this.boxId.toString());  //box name
-
-    /*not able to add (click) to button due to which not able
-     to proceed forward with this approach*/ 
-    //button.setAttribute('(click)','onBoxClicked()');
-
-    button.innerHTML =  'Box' + this.boxId;
-
-    this.renderer.appendChild(this.div.nativeElement, button); 
+  onAdd() {    
+    myBox();
   }  
 
-  onBoxClicked(){
-    console.log('click event');
-    /* check which button was clicked and add transform/animation effect to move elements 
-      when the keys are pressed both w,a,s,d and arrow key. 
-       Keycodes for arrows are followed:
-        left = 37
-        up = 38
-        right = 39
-        down = 40
-      Also listen for delete key press event to delete the element.
-        */
+  onChecked(checkStatus:boolean){
+    this.checkboxFlag = checkStatus;
+    if(checkStatus){
+      console.log(this.checkboxFlag);
+    }
+    else{
+      console.log(this.checkboxFlag);      
+    }    
   }
 
 }
